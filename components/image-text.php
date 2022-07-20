dd2374566c92a6cff9df6b2cf346948a19e0070e
sandboxSonny <sonnyadams57@icloud.co.uk>
2022-07-20 20:01:24 +0100

    Added content alignment to the image with text section

--- components/image-text.php
+++ components/image-text.php
@@ -4,6 +4,7 @@
     $background = get_sub_field('background');
     $image = get_sub_field('image');
     $image_size = 'large';
+    $content_align = get_sub_field('content_align');
     $title = get_sub_field('title');
     $content = get_sub_field('text');
     $button_link = get_sub_field('button_link');
@@ -18,12 +19,14 @@
                             echo wp_get_attachment_image($image, $image_size, "", array( "class" => "img-fluid" ));
                         }
                     echo '</div>';
-                    echo '<div class="col-md-6 image-text_content">';
-                        echo '<h2>' . $title . '</h2>';
-                        echo '<p>' . $content . '</p>';
-                        if($button_link) {
-                            echo '<a class="btn btn-primary" href="' . $button_link . '">' . $button_text . '</a>';
-                        }
+                    echo '<div class="col-md-6 image-text_content image-text_content--' . $content_align . '">';
+                        echo '<div>';
+                            echo '<h2>' . $title . '</h2>';
+                            echo '<p>' . $content . '</p>';
+                            if($button_link) {
+                                echo '<a class="btn btn-primary" href="' . $button_link . '">' . $button_text . '</a>';
+                            }
+                        echo '</div>';
                     echo '</div>';
                 echo '</div>';
             echo '</div>';
