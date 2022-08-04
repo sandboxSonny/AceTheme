e0709a8e80a315760f6be1aad7b6ccf6ef83633d
sandboxSonny <sonnyadams57@icloud.co.uk>
2022-08-04 20:34:46 +0100

    Built in the ability to use custom fonts from Google Fonts

--- header.php
+++ header.php
@@ -6,6 +6,18 @@
     <?php do_action('wp_head'); ?>
     <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/dist/styles/common.min.css">
     <?php get_template_part('includes/include', 'css-variables'); ?>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <?php
+      $font = get_field('font_family', 'option');
+      if ($font) {
+        $font = str_replace(' ', '+', $font);
+      } else {
+        $font = 'Mulish';
+      }
+      echo '<link href="https://fonts.googleapis.com/css2?family=' . $font . ':wght@300;400;600;700&display=swap" rel="stylesheet">';
+    ?>
   </head>
   <body>
 
