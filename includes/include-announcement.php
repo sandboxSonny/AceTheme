a58a534d7927e9d42057dd6223546cde37a71ed8
sandboxSonny <sonnyadams57@icloud.co.uk>
2022-08-04 20:12:08 +0100

    Added the ability to choose the background of the announcement bar

--- includes/include-announcement.php
+++ includes/include-announcement.php
@@ -1,11 +1,18 @@
 <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/dist/styles/component-announcement.css">
 
-<div class="announcement">
+<?php
+    $enable_details = get_field('announcement_enable_details', 'option');
+    $email = get_field('email', 'option');
+    $primary_number = get_field('primary_number', 'option');
+    $secondary_number = get_field('secondary_number', 'option');
+    $content = get_field('announcement_content', 'option');
+    $background = get_field('announcement_background', 'option');
+?>
+
+<div class="announcement bg-<?php echo $background; ?>">
     <div class="container">
         <div class="row align-items-center">
             <?php
-                $content = get_field('announcement_content', 'option');
-                $enable_details = get_field('announcement_enable_details', 'option');
                 if ($content) {
                     echo '<div class="announcement_content">';
                         echo '<span>' . $content . '</span>';
@@ -14,9 +21,6 @@
 
                 if ($enable_details) {
                     echo '<div class="col announcement_details flex-1">';
-                        $email = get_field('email', 'option');
-                        $primary_number = get_field('primary_number', 'option');
-                        $secondary_number = get_field('secondary_number', 'option');
                         if ($email) {
                             echo '<a href="mailto:' . $email  .  '">' . $email . '</a>';
                         }
