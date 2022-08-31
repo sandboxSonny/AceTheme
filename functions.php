71a2532ec3b467d851ef179dd34b7b17361142b4
sandboxSonny <sonnyadams57@icloud.com>
2022-08-31 19:08:41 +0100

    Removed some Guttenburg stylesheets that are included by default

--- functions.php
+++ functions.php
@@ -1,5 +1,6 @@
 <?php
-// Action Hooks
+
+// Add Theme Menus 
 add_action( 'wp_head', 'ag_head' );
 add_action( 'after_setup_theme', 'register_custom_nav_menus' );
 function register_custom_nav_menus() {
@@ -8,8 +9,9 @@ function register_custom_nav_menus() {
 		'footer_menu' => 'Footer Menu',
 	) );
 }
-
 add_theme_support('post-thumbnails');
+
+// Add Custom Post Types from ACF
 $post_types = have_rows('custom_post_types', 'option');
 if ($post_types) {
 	while ( have_rows('custom_post_types', 'option') ) : the_row();
@@ -51,7 +53,7 @@ if ($post_types) {
 	endwhile;
 }
 
-
+// Add option pages to the Wordpress admin 
 if( function_exists('acf_add_options_page') ) {
 	acf_add_options_page(array(
 		'page_title' 	=> 'General Settings',
@@ -112,8 +114,17 @@ if( function_exists('acf_add_options_page') ) {
 	));
 }
 
+// Remove Start to the Archive Titles 
 function change_my_title( $title ){
     $title = str_replace('Archives: ', '', $title) ;
     return $title;
 }
 add_filter( "get_the_archive_title", "change_my_title" );
+
+//Remove Gutenberg Block Library CSS from loading on the frontend
+function smartwp_remove_wp_block_library_css(){
+	wp_dequeue_style( 'wp-block-library' );
+	wp_dequeue_style( 'wp-block-library-theme' );
+	wp_dequeue_style( 'wc-blocks-style' ); // Remove WooCommerce block CSS
+} 
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css', 100 );
